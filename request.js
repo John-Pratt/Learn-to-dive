0fd02ac2e9d753861a5308fc38177128d3d3e093
John <johnw_pratt@outlook.com>
2022-08-17 10:11:26 -0500

    Practicing vanilla js

--- request.js
+++ request.js
@@ -12,3 +12,21 @@ console.log(numb2);
 console.log(numb1 + numb2);
 console.log(hello);
 console.log(hello + numb1);
+
+let stringer = "This is a string";
+
+console.log(stringer);
+
+let x = 2;
+let y = 2;
+let z = 4;
+console.log((x + y) == z);
+
+let person = {
+    name: 'Wyatt',
+    age: 37,
+    married: false
+}
+
+console.log(person.age)
+
